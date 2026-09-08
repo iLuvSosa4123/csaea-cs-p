44d7578343ecaf812e8255758c7a69a307d05d6f
iLuvSosa4123 <alexnikif2012@gmail.com>
2026-09-08 18:23:05 +0000

    My first commit

---
+++ crash-course.py
@@ -0,0 +1,30 @@
+# comment
+
+# here 
+# is 
+# a 
+# comment (control dash /)
+print("hello world")
+# variable declarations and types
+a = 4       #integer
+b = 5.5     #float
+c = "CSAEA" #string
+d = False   #boolean
+
+print(a, b, c, d)
+
+# OPERATORS
+# + - / * (modulus, divides and gives you a remainder %) (expononent **) (integer division //)
+# += -= /= 
+e = 3 - 1 
+print (e)
+e += 7 
+print(e)
+# f-string
+
+print(f"e is equal to {e}")
+
+e -= 7
+e += 12 
+
+print(f"e is NOW eqaul to {e}")
