18122223385cc36a012c3686f0c6356a6651e06c
iLuvSosa4123 <alexnikif2012@gmail.com>
2026-09-10 16:26:28 +0000

    cashcourse day 2 complete

--- crash-course.py
+++ crash-course.py
@@ -1,5 +1,6 @@
-# comment
+import math 
 
+# comment
 # here 
 # is 
 # a 
@@ -28,3 +29,60 @@ e -= 7
 e += 12 
 
 print(f"e is NOW eqaul to {e}")
+
+# COMPAIRSONS (booleans, which always return True of False)
+
+# < >   <=  >=  ==  !+
+
+print ( 4 < 5 ) 
+print(7 == 4)
+print ( 1 != 2 )
+
+isEqual = "Yes" != "YES"
+print(isEqual)
+
+# LOGICAL OPERATORS 
+# In order of precedence: not and or 
+
+f = False
+t = True 
+#predict output, Dont run)
+print(not f)
+print(f and t)
+print(f or t)
+print(f or t and not f)
+
+#Truth table T F and False
+#            F T and F
+#            T T and F
+#            F F and F 
+# if the false is there its always going to be false even if there is a true hence if there is a double T then its true 
+# Table truth or version: T F or True
+#                         F T or true 
+#                         T T or true
+#                         F F or False 
+#                       THEY CAN BE EITHER OR THE T F AND F T CAN ALSO BE FALSE but its perfered to be True 
+
+# CASTING () 
+g = int(5.9) 
+print(g) 
+
+s1 = "Goodnight"
+s2 = " and " 
+s3 = "Goodbye"
+end = s1 + s2 + s3 # concatenation with + 
+end += ", Cowboy."
+
+print(end + "\n")
+
+# MATH LIBRARY 
+# max, min, square root, 
+# to use math you need to "call it" sqrt = square root 
+# ceil = round up
+# floor = round down
+
+print(math.sqrt(14))
+
+print(math.ceil(3.65))
+print(math.floor(8.94))
+print(math.pow(2, 4)) 
