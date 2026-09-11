37e07e04452f167682acc3085dea16e2189cb087
iLuvSosa4123 <alexnikif2012@gmail.com>
2026-09-11 17:20:05 +0000

    Updates Crash Course Day three complete

--- crash-course.py
+++ crash-course.py
@@ -86,3 +86,60 @@ print(math.sqrt(14))
 print(math.ceil(3.65))
 print(math.floor(8.94))
 print(math.pow(2, 4)) 
+
+# CONDITIONALS
+
+# if    elif    else 
+
+t = True
+f = False
+
+if f:
+    print("Reached the first condition")
+elif t:
+    print("Reached Second Condition")
+else: 
+    print("Reached else")
+
+if 100 > 500 and 99 == 1:
+    print("Reached the first condition")
+elif 6 == 7 or 5 < 2:
+    print("Reached second condition")
+elif 9 == 10 and 7 > 3:
+    print("Reached third condition")
+else: 
+    print("Reached else")
+
+# LISTS 
+# A list can hold any tpye, and can grow or shrink at any time 
+
+#index: 0   1   2  3  4
+nums = [34, 52, 3, 64,32]
+
+print(nums)
+print (nums[3])
+print (nums[0])
+print(nums[-1])
+print(nums[-3])
+
+print(nums[0] + nums[2]) 
+
+nums[0] = 64
+print(nums) 
+
+# LIST METHODS 
+# Special built-in methods 
+
+words = []
+
+words.append("Word 1")
+words.append("Word 2")
+words.append("Word 3")
+print(words)
+
+words.remove("Word 1")
+words.insert(0, "Word 4")
+words[1] = "Word 5"
+length = len(words)
+print(words)
+print(length)
